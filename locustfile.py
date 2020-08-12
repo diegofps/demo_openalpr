cf270d7b001faa09db6f5e4ca1346dc9847e74b1
Diego Souza <diego@wespa.com.br>
2020-08-11 22:17:53 -0300

    Cleaning locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,8 @@
-import random
 from locust import HttpUser, task, between
 from glob import glob
+
+import random
+import os
 
 class OpenALPRUser(HttpUser):
     wait_time = between(5, 9)
@@ -8,13 +10,12 @@ class OpenALPRUser(HttpUser):
     @task
     def recognize(self):
         r = self.client.post("/recognize", files={'imagefile': self.filedata})
-        print(r.status_code, r.text)
+        #print(r.status_code, r.text)
     
     def on_start(self):
         self.filepaths = glob("./dataset/*")
-        self.filepath = "/home/diego/Sources/openalpr/image_0001.jpg"
+        self.filepath = os.path.expanduser("~/Sources/openalpr/image_0001.jpg")
+        
         with open(self.filepath, "rb") as fin:
             self.filedata = fin.read()
-        # self.client.post("/login", {"username":"foo", "password":"bar"})
-        pass
 
