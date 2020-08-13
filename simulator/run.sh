db4222b14174c0f7fd3d59c905aee3da8dc5400d
Diego Souza <diego@wespa.com.br>
2020-08-13 14:19:37 -0300

    Refactoring simultator/run.sh

--- simulator/run.sh
+++ simulator/run.sh
@@ -1,67 +1,40 @@
 #!/bin/sh
 
-
 PREFIX="output"
-STABILIZE_TIME=60
-COLLECT_TIME=180
+STABILITY_TIME=30
+COLLECT_TIME=30
 
-echo "Starting server"
-locust -f locustfile.py &
-sleep 5
+function start
+{
+    echo "Starting server"
+    locust -f locustfile.py &
+    sleep 5
 
-################################################################################
+    curl -d user_count=100 -d hatch_rate=30 -d host=http://10.20.31.92:4570/forward http://10.20.31.26:8089/swarm
+    echo "" > results.csv
+}
 
-echo "Running experiment for 300 users"
-curl -d user_count=300 -d hatch_rate=30 -d host=http://10.20.31.92:4570/forward http://10.20.31.26:8089/swarm
-sleep $STABILIZE_TIME
+function measure
+{
+    QTD=$1
+    FILENAME="${PREFIX}_${QTD}.csv"
 
-echo "Stability time finished, measuring"
-curl http://10.20.31.26:8089/stats/reset
-sleep $COLLECT_TIME
-wget http://10.20.31.26:8089/stats/requests/csv -O $PREFIX_300.csv
+    echo "Running experiment for ${QTD} users"
+    curl -d user_count=$QTD -d hatch_rate=30 http://10.20.31.26:8089/swarm
+    sleep $STABILITY_TIME
 
-################################################################################
+    echo "Stability time finished, measuring"
+    curl http://10.20.31.26:8089/stats/reset
+    sleep $COLLECT_TIME
+    wget http://10.20.31.26:8089/stats/requests/csv -O ${FILENAME}
 
-echo "Running experiment for 600 users"
-curl -d user_count=600 -d hatch_rate=30 http://10.20.31.26:8089/swarm
-sleep $STABILIZE_TIME
+    q -H -d ',' "select \"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from ${FILENAME} limit 1" -O >> results.csv
+}
 
-echo "Stability time finished, measuring"
-curl http://10.20.31.26:8089/stats/reset
-sleep $COLLECT_TIME
-wget http://10.20.31.26:8089/stats/requests/csv -O $PREFIX_600.csv
+start
 
-################################################################################
-
-echo "Running experiment for 900 users"
-curl -d user_count=900 -d hatch_rate=30 http://10.20.31.26:8089/swarm
-sleep $STABILIZE_TIME
-
-echo "Stability time finished, measuring"
-curl http://10.20.31.26:8089/stats/reset
-sleep $COLLECT_TIME
-wget http://10.20.31.26:8089/stats/requests/csv -O $PREFIX_900.csv
-
-################################################################################
-
-echo "Running experiment for 1200 users"
-curl -d user_count=1200 -d hatch_rate=30 http://10.20.31.26:8089/swarm
-sleep $STABILIZE_TIME
-
-echo "Stability time finished, measuring"
-curl http://10.20.31.26:8089/stats/reset
-sleep $COLLECT_TIME
-wget http://10.20.31.26:8089/stats/requests/csv -O $PREFIX_1200.csv
-
-################################################################################
-
-echo "" > results.csv
-
-q -H -d ',' "select \"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from $PREFIX_300.csv limit 1" -O >> results.csv
-
-q -H -d ',' "select \"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from $PREFIX_600.csv limit 1" >> results.csv
-
-q -H -d ',' "select \"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from $PREFIX_900.csv limit 1" >> results.csv
-
-q -H -d ',' "select \"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from $PREFIX_1200.csv limit 1" >> results.csv
+measure 300
+measure 600
+measure 900
+measure 1200
 
