c8792b7a5fa8733f53244ebb40c82bc2adf1a88e
Diego Souza <diego@wespa.com.br>
2020-08-13 14:16:11 -0700

    Adding deploy scripts

--- simulator/simulate.sh
+++ simulator/simulate.sh
@@ -3,7 +3,9 @@
 OUTPUTFILE="result.csv"
 PREFIX="output"
 STABILITY_TIME=30
-COLLECT_TIME=30
+COLLECT_TIME=300
+HOST="http://10.20.31.92:4570/forward"
+#HOST="http://10.20.31.92:4568"
 
 function start
 {
@@ -12,7 +14,7 @@ function start
     locust -f locustfile.py &
     sleep 5
 
-    curl -d user_count=100 -d hatch_rate=30 -d host=http://10.20.31.92:4570/forward http://10.20.31.26:8089/swarm
+    curl -d user_count=100 -d hatch_rate=30 -d host=$HOST http://10.20.31.26:8089/swarm
     rm -f $OUTPUTFILE
     mkdir tmp
 }
@@ -34,6 +36,11 @@ function measure
     q -H -d ',' "select $QTD as \"Users\",\"Request Count\",\"Failure Count\",\"Median Response Time\",\"Average Response Time\",0 as \"Var in Avg Response Time\",\"Min Response Time\",\"Max Response Time\",\"Average Content Size\",\"Requests/s\",0 as \"Var in Requests/s\",\"Failures/s\",\"50%\",\"66%\",\"75%\",\"80%\",\"90%\",\"95%\",\"98%\",\"99%\",\"99.9%\",\"99.99%\",\"99.999%\",\"100%\" from ${FILENAME} limit 1" >> $OUTPUTFILE
 }
 
+function terminate
+{
+    killall locust
+}
+
 start
 
 measure 300
@@ -41,3 +48,7 @@ measure 600
 measure 900
 measure 1200
 
+terminate
+
+echo "Experiment completed"
+sleep 2
