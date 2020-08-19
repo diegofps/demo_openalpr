0785dfa8ee830f4912252dcfa138ef96c814c60c
Diego Souza <diego@wespa.com.br>
2020-08-18 19:10:52 -0700

    Bugfix in simulate_all.sh

--- tools/simulate_all.sh
+++ tools/simulate_all.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_default.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_default.sh'
 ./simulate.sh result_default.csv
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_w.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_w.sh'
 ./simulate.sh hybrid_w.csv
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_r.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_r.sh'
 ./simulate.sh hybrid_r.csv
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_wob.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_wob.sh'
 ./simulate.sh hybrid_wob.csv
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_awob.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_awob.sh'
 ./simulate.sh hybrid_awob.csv
 
-ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_m.sh
+ssh locust@10.20.31.92 'cd /home/ngd/Sources/demo_openalpr/tools && sudo ./expose_m.sh'
 ./simulate.sh hybrid_m.csv
 
