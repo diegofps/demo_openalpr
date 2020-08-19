2ad52fc063c684a9db74823769791ca8c320c207
Diego Souza <diego@wespa.com.br>
2020-08-18 19:05:16 -0700

    simulate_all.sh now configures a remote machine

--- tools/simulate_all.sh
+++ tools/simulate_all.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-./expose_default.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_default.sh
 ./simulate.sh result_default.csv
 
-./expose_w.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_w.sh
 ./simulate.sh hybrid_w.csv
 
-./expose_r.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_r.sh
 ./simulate.sh hybrid_r.csv
 
-./expose_wob.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_wob.sh
 ./simulate.sh hybrid_wob.csv
 
-./expose_awob.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_awob.sh
 ./simulate.sh hybrid_awob.csv
 
-./expose_m.sh
+ssh locust@10.20.31.92 sudo /home/ngd/Sources/demo_openalpr/tools/expose_m.sh
 ./simulate.sh hybrid_m.csv
 
