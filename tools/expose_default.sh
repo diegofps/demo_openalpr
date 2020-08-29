03c674dae12020ce2a4b52fa2662b3d9198638b7
Diego Souza <diego@wespa.com.br>
2020-08-29 13:21:47 -0300

    Enhancing status

--- tools/expose_default.sh
+++ tools/expose_default.sh
@@ -7,3 +7,7 @@
 sudo kubectl apply -f deployment/svc-openalpr.yaml
 
 ./wait_run.sh
+
+SVC_NAME=$(ssh locust@$TARGET "sudo kubectl get services | grep openalpr- | awk '{print \$1}'")
+NODE_PORT=$(ssh locust@$TARGET "sudo kubectl get services/$SVC_NAME -o go-template='{{(index .spec.ports 0).nodePort}}'")
+echo "default proxy, got svc=$SVC_NAME and port=$NODE_PORT"
--- tools/status.sh
+++ tools/status.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo kubectl get deployments
-sudo kubectl get services
+echo -e "\n---Deployments---"
+sudo kubectl get deployments -o wide
+
+echo -e "\n---Services---"
+sudo kubectl get services -o wide
+
+echo -e "\n---Nodes---"
+sudo kubectl get nodes -o wide
+
+echo -e "\n---Nodes SSH---"
+parallel-ssh -h ~/nodes -i -t 0 "echo alive"
