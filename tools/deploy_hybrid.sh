b6d8d116514d354141a02a7e69405864900ee929
Diego Souza <diegofpsouza@gmail.com>
2020-08-28 20:00:19 -0700

    Setting fixed number of pods on each deploy type

--- tools/deploy_hybrid.sh
+++ tools/deploy_hybrid.sh
@@ -4,7 +4,8 @@
 ./undeploy_app.sh
 
 # Create the desired proxy, cluster mode and expose it 
-sudo kubectl apply -f ./deployment/app-hybrid.yaml
+sudo kubectl apply -f ./deployment/app-csd.yaml
+sudo kubectl apply -f ./deployment/app-perl.yaml
 
 # Wait for all pods to start
 ./wait_run.sh
